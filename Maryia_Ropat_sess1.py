1f474b49f4ff572c568b70102e99d1267b4d7a8b
Mropat <35531751+Mropat@users.noreply.github.com>
2018-01-17 18:15:33 +0100

    Add files via upload

---
+++ Maryia_Ropat_sess1.py
@@ -0,0 +1,29 @@
+def printHello():
+    print ("Hello, World")
+
+
+def readAndSaveFile(filename):
+	filehandle = open(filename, 'r')
+	lines = filehandle.readlines()
+	head = lines[0]
+	tail = lines[-1]
+	filending =""
+	if "." in filename:
+		filename, filending = filename.split(".")
+		filending = "."+filending
+	writefile = open(filename +"_headtail"+filending, 'w')
+	writefile.write(head + tail)
+	
+
+def printTwoStrings(stringA, stringB):
+    print (stringA, stringB)
+	
+if __name__=="__main__":
+	printHello()
+	readAndSaveFile("science.txt")
+	printTwoStrings("please enter", "your strings")
+		
+
+
+
+
