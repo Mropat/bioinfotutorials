18160b8eb01d1b41413810c103d68d045a33c098
Maria <mariaropat@gmail.com>
2018-01-17 19:49:57 +0100

    boii

--- maryia_ropat_sess2.py
+++ maryia_ropat_sess2.py
@@ -1,31 +1,29 @@
 def compute_gc(sequence):
     allcount = len(sequence)
-    gccount = sequence.count("GC", 0, -1)
-    print (gccount*2/allcount)
-    
+    if allcount == 0:
+        return 0
+    gccount = sequence.count("GC")
+    return gccount*2/allcount    
     
 
 def dist(a, b):
-    counta = a.count("", 0, -1)
-    countb = b.count("", 0, -1)
+    counta = len(a)
+    countb = len(b)
     distance = 0
     if counta != countb:
         print ("sequence lengths don't match")
-    for x in range (0, counta):
+        return
+    for x in range (counta):
         if a[x] != b[x]:
             distance = distance + 1
-        x = x + 1
-    print(distance)
-    
-    
+    return distance    
 
     
 def score(a, b):
     if a == b:
-        print("1")
+        return 1
     else: 
-        print ("0")
-        
+        return 0        
         
     
 def matchStrings(stringA, stringB):
@@ -34,27 +32,26 @@ def matchStrings(stringA, stringB):
     else:
         matchl = len(stringA)
     match = 0
-    for z in range (0, matchl):
-        if stringA[z] == stringB[z]:
-            match = match +1
-        z = z + 1
-    print (match)
+    for z in range (matchl):        
+        match += score(stringA[z],stringB[z])
+    return match
     
+
 if __name__=="__main__":
-    inseq = input("Insert sequence to determine GC content")
-    compute_gc(inseq)
+    inseq = input("Insert sequence to determine GC content: ")
+    print(compute_gc(inseq))    
     
-    distseq = input (" To calculate distance, input fist fequence")
-    distseq2 = input ("Input second sequence")
-    dist(distseq, distseq2)
+    distseq = input("To calculate distance, input fist fequence: ")
+    distseq2 = input("Input second sequence: ")
+    print(dist(distseq, distseq2))
     
-    sca = input ("Input a single character")
-    scb = input ("Input second character")
-    score(sca, scb)
+    sca = input("Input a single character: ")
+    scb = input("Input second character: ")
+    print(score(sca, scb))
     
-    ms1 = input ("Input first string to count matches")
-    ms2 = input ("Input second string")
-    matchStrings(ms1, ms2)
+    ms1 = input("Input first string to count matches: ")
+    ms2 = input("Input second string: ")
+    print(matchStrings(ms1, ms2))
      
                 
     
