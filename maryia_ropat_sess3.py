1c148bec7bd013cb3caa3dd4a2ee362362bfa053
Maria <mariaropat@gmail.com>
2018-01-18 09:07:46 +0100

    new lab

---
+++ maryia_ropat_sess3.py
@@ -0,0 +1,11 @@
+def is_protein(seq):
+    return False
+
+def longest_line(file_name):
+    return 42
+
+def parse_fasta(file_name):
+    return ['PEPTIDE', 'PEPT-KE']
+
+def write_complement(input_file_name, output_file_name):
+    pass
