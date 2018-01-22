86950c1895ae6a90915ccb5d0506f4568130754a
Maria <mariaropat@gmail.com>
2018-01-22 18:19:14 +0100

    selfedit and tests

--- Sess4/aafasta.py
+++ Sess4/aafasta.py
@@ -1,24 +1,19 @@
-def unique_aa(filename)
+def unique_aa(filename):
     aa_list =[]
     with open(filename, "r") as fh:
-        for x , 1 in enumerate(fh):
-            line_id = fh.readline[x]
-            if ">" in line_id[0]:
+        for line in fh.readlines():
+            if ">" in line[0]:
                 continue
             else:
-                for x, 1 in enumerate(line_id)           
-                    if line_id[x] in aa_list:
-                        continue
-                    else:
-                        aa_list = aa_list.append(typeline_id[x])
-
-            
-                
-                
-
-           
-        
-        
-    
-
-unique_aa("uniref90.fasta")
+                for char in line:
+                    if char.isalpha():
+                        if char not in aa_list:
+                            aa_list.append(char)
+    popin_line = "aa_list =" + "{}\n".format(aa_list)
+    with open ("aafasta2.py", "r+") as selfedit:
+        insert_c = selfedit.readlines()
+        insert_c.insert(5, popin_line)
+        selfedit.seek(0)
+        selfedit.writelines(insert_c)
+if __name__ == "__main__":
+    unique_aa("example.fasta")
